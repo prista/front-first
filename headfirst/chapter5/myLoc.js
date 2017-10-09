fb6266d4749ca0fc185f4c48e7663c0d2a34f5f5
victor.burba <victor.burba@azati.com>
2017-10-09 18:17:55 +0300

    add watch and clear buttons

--- headfirst/chapter5/myLoc.js
+++ headfirst/chapter5/myLoc.js
@@ -1,10 +1,39 @@
-window.onload = getMyLocation();
+var watchId = null;
+
+var homeCoords = {
+  latitude: 53.641901,
+  longitude: 23.844813
+};
+
+var options = {enableHighAccuracy: true, timeout: 100, maximumAge: 0};
+
+window.onload = getMyLocation;
 
 function getMyLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+    var watchButton = document.getElementById("watch");
+    watchButton.onclick = watchLocation;
+    var clearWatchButton = document.getElementById("clearWatch");
+    clearWatchButton.onclick = clearWatch;
   } else {
     alert("Oops, no geolocation support");
+  }
+}
+
+function watchLocation() {
+  // argumets: success handler and error handler
+  //watchId = navigator.geolocation.watchPosition(displayLocation, displayError,
+  //  {timeout:5000});
+
+    watchId = navigator.geolocation.watchPosition(displayLocation, displayError,
+    options);
+}
+
+function clearWatch() {
+  if (watchId) {
+
+    navigator.geolocation.clearWatch(watchId);
+    watchId = null;
   }
 }
 
@@ -15,6 +44,19 @@ function displayLocation(position) {
 
   var div = document.getElementById("location");
   div.innerHTML = "You are at Latitude: " + latitude + ", Longitude: " + longitude;
+  div.innerHTML += " (found in " + options.timeout + " milliseconds)";
+  // если удалось определить локацию, сообщаем сколько времени это заняло
+  div.innerHTML += " (with " + position.coords.accuracy + " meters accuracy)";
+
+  var km = computeDistance(position.coords, homeCoords);
+  var distance = document.getElementById("distance");
+  distance.innerHTML = "You are " + km + " km from the house";
+
+  // если функция showMap еще не вызывалась, следует вызвать ее;
+  // в противном случае ее не нужно будет вызывать при каждом вызове displayLocation
+  if (map == null) {
+    showMap(position.coords);
+  }
 }
 
 // error handler
@@ -27,7 +69,6 @@ function displayError(error) {
       3: "Request timed out"
   };
 
-
   //В случае с ошибками О и 2 иногда
   //в свойстве error.message присутствует дополнительная инфа
   //поэтому мы добавляем его в нашу строку error.Message.
@@ -38,4 +79,78 @@ function displayError(error) {
 
   var div = document.getElementById("location");
   div.innerHTML = errorMessage;
+
+  // если не удалось определить местоположение, будем увеличивать
+  // таймаут, также уведомление будет появляться о повторной попытке
+  options.timeout +=100;
+  navigator.geolocation.getCurrentPosition(
+    displayLocation,
+    displayError,
+    options);
+  div.innerHTML += "... checking again with timeout=" + options.timeout;
+}
+
+function computeDistance(startCoords, destCoords) {
+  var startLatRads = degreesToRadians(startCoords.latitude);
+  var startLongRads = degreesToRadians(startCoords.longitude);
+  var destLatRads = degreesToRadians(destCoords.latitude);
+  var destLongRads = degreesToRadians(destCoords.longitude);
+
+  var Radius = 6371;
+  var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) +
+    Math.cos(startLatRads) * Math.cos(destLatRads) *
+    Math.cos(startLongRads - destLongRads)) * Radius;
+
+  // возвращает расстояние в километрах
+  return distance;
+}
+
+function degreesToRadians(degrees) {
+  var radians = (degrees * Math.PI)/180;
+  return radians;
+}
+
+// google maps
+
+var map;
+
+function showMap(coords) {
+  var googleLatAndLong = new google.maps.LatLng(coords.latitude,coords.longitude);
+
+  var mapOptions = {
+  zoom: 10,
+  center: googleLatAndLong,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var mapDiv = document.getElementById("map");
+  map = new google.maps.Map(mapDiv, mapOptions);
+
+  var title = "Your Location";
+  var content = "You are here: " + coords.latitude + ", " + coords.longitude;
+  addMarker(map, googleLatAndLong, title, content);
+}
+
+//add marker
+function addMarker(map, latlong, title, content) {
+  var markerOptions = {
+    position: latlong,
+    map: map,
+    title: title,
+    clickable: true
+  };
+
+  var marker = new google.maps.Marker(markerOptions);
+
+  // Создадим информационное окно
+  var infoWindowOptions = {
+    content: content,
+    position: latlong
+  };
+
+  var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+
+  google.maps.event.addListener(marker, "click", function() {
+    infoWindow.open(map);
+  })
 }
